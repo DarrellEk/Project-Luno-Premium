840d920ad9d5a9b7eaeb630a1d3aa9d832e9a371
Darrell <darrellstash@gmail.com>
2023-10-25 17:37:43 +0800

    Cleaned up code, simplified

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-/*
-COUNTRY_CURRENCY_API = "lGHdeV8KjpkpraDC2kAof04MhJSM82aL"
-COUNTRY_CURRENCY_URL = "https://api.apilayer.com/fixer/convert?to=myr&from=usd&amount=1"
-LUNO_API_KEY = "Ww2eWhQa7sYD1GFz27BbEXCqePBltSClxJPh7aunv10"
-LUNO_API_LINK = "kb3tjhkutmwvn"
-BINANCE_API_SECRET = "FQX00oPFrRUnA2qPzce53RktZdwjrm36OXevuypahdskjjl5QOMYj9mRzlyUqjTw"
-BINANCE_API_KEY = "geD4ItFUAQ0SZxtXHWkCTX4J1DgVbiyPLeLtaI6yZCMhj9lOwlK9KNExl7jZukI2"
-*/
-
-
-
+const Binance = require('node-binance-api')
 require('dotenv').config()
 
 // to fetch BTCMYR price on Luno
@@ -17,7 +7,6 @@ async function fetchBTCMYR(){
     try {
         const response = await fetch(lunoBTCMYR)
         const data = await response.json()
-        // console.log(data.last_trade)
         return +data.last_trade
     } catch(error){
         console.error("error", error)
@@ -26,19 +15,16 @@ async function fetchBTCMYR(){
 
 // to fetch currency conversion from USD to MYR
 async function currencyCountry(){
-    const currencyCountryAPI = process.env.CURRENCY_API
-    const currencyCountryURL = process.env.CURRENCY_URL
     try {
-        const response = await fetch(currencyCountryURL, {
+        const response = await fetch(process.env.CURRENCY_URL, {
             method: "GET", 
             headers: {
-                "apikey": currencyCountryAPI
+                "apikey": process.env.CURRENCY_API
             }
         })
         if (response.ok){
             const data = await response.json()
             const rate = data.result
-            // console.log(rate)
             return +rate
         }
     } catch (error){
@@ -52,7 +38,6 @@ async function calculateUSDLuno(){
         const BTCMYRvalue = await fetchBTCMYR() 
         const currencyUSDMYR = await currencyCountry() 
         const result = BTCMYRvalue * currencyUSDMYR
-        // console.log(result) // number 
         return +result
     } catch(error){
         console.error("error", error)
@@ -60,13 +45,10 @@ async function calculateUSDLuno(){
 }
 
 // to get BTCUSD in binance (this) 
-const Binance = require('node-binance-api')
-
 async function getBTCBUSD() {
   try {
     const binance = new Binance(); //not the same as the fetch function
     const ticker = await binance.prices();
-    // console.log(ticker.BTCBUSD)
     return +ticker.BTCBUSD;
   } catch (err) {
     return NaN;
@@ -79,7 +61,6 @@ async function priceDifference(){
         const priceLunoBTC = await calculateUSDLuno()
         const priceBinanceBTC = await getBTCBUSD()
         const difference = priceLunoBTC - priceBinanceBTC 
-        // console.log(difference)
         return +difference
     } catch(error){
         console.error("error", error)
@@ -92,7 +73,6 @@ async function lunoPremium(){
         const difference = await priceDifference()
         const BTCluno = await calculateUSDLuno()
         const percentage = (difference / BTCluno) * 100
-        // console.log(percentage)
         return +percentage
     } catch(error){
         console.error(error, "error")
@@ -109,12 +89,12 @@ async function printmenu(){
         const binanceBTCUSD = await getBTCBUSD()
         const difference = await priceDifference()
         const premium = await lunoPremium()
-        console.log(`BTCMYR price on LUNO:          MYR ${lunoBTCMYR}`)
-        console.log(`USDMYR:                            ${currency}`)
-        console.log(`BTCUSD price on LUNO:          USD ${lunoBTCUSD}`)
-        console.log(`BTCUSD price on Binance:       USD ${binanceBTCUSD}`)
-        console.log(`Price Difference:              USD ${difference}`)
-        console.log(`Luno Premium:                      ${premium}%`)
+        console.log(`BTCMYR price on LUNO:`.padEnd(30)       + `MYR ${lunoBTCMYR}`)
+        console.log(`USDMYR:`.padEnd(30)                     +     `${currency}`)
+        console.log(`BTCUSD price on LUNO:`.padEnd(30)       + `USD ${lunoBTCUSD}`)
+        console.log(`BTCUSD price on Binance:`.padEnd(30)    + `USD ${binanceBTCUSD}`)
+        console.log(`Price Difference:`.padEnd(30)           + `USD ${difference}`)
+        console.log(`Luno Premium:`.padEnd(30)               +      `${premium}%`)
     } catch (error){
         console.error("error", error)
     }
